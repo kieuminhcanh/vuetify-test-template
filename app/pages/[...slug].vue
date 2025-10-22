ebcb2b14d301ad62e7839ac8d175d893764896c8
Ken Kieu <minhcanhdn0704@gmail.com>
2025-10-22 11:56:42 -0700

    Initial commit

---
+++ app/pages/[...slug].vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+  import * as components from "vuetify/components"
+  import * as labs from "vuetify/labs/components"
+
+  const route = useRoute()
+  const { data: page } = await useAsyncData(route.path, () => {
+    return queryCollection('content').path(route.path).first()
+  })
+
+</script>
+
+<template>
+  
+  <ContentRenderer v-if="page"
+    :value="page" 
+    :components="{ ...components, ...labs }"
+    />
+
+</template>
+
+<style></style>
---
+++ content/index.md
@@ -0,0 +1,15 @@
+# My First Page
+
+Here is some content.
+
+::VCard{title="Card Title"}
+This is a card component.
+:::VCard{title="Sub Card Title"}
+This is a sub card component.
+::::VCard{title="Sub Sub Card Title"}
+::::
+This is a sub card component.
+:::
+::
+
+::VBtn{text="Click Me"}
